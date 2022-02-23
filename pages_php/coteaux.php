b7af7f621111c8e6830f16fd73466efe62bc0527
Kilouhardi <98669752+Kilouhardi@users.noreply.github.com>
2022-02-23 09:02:42 +0100

    premier test php

---
+++ pages_php/coteaux.php
@@ -0,0 +1,26 @@
+<?php
+
+//dans chaque première cases du tableau on va voir un morceau d'html qui représente un article en francais
+//puis dans la seconde case la même chose mais en anglais
+
+$article = array(
+
+"
+<h2>Coteaux de Champagne</h2>
+<p>
+        Depuis 2015, la région Champagne est inscrite au patrimoine mondial de l’UNESCO. Est inscrit à ce patrimoine ses caves, ses maisons et ses coteaux. En effet c’est bien grâce à sa géographie exceptionnelle que la Champagne a mérité sa place auprès d’autres lieux d'exceptions.
+    <br>    Pour tous ceux qui souhaitent voir les merveilles que propose cette région française, cet article est fait pour vous.
+    <br>    Parmi tous les coteaux qui parcourent la vallée de la marne, trois ont été choisis pour représenter la région auprès du monde: les coteaux historiques de Hautvillers, Aÿ et Mareuil-sur-Aÿ. 
+</p>
+<h3>La vallée de la marne</h3>
+<p>
+    Cette vallée qui s’étend de Tours-sur-Marne à Dormans traverse le cœur historique du vignoble champenois et possède en son sein une partie des coteaux les plus connus de la Champagne. On y trouve la culture de la vigne de manière omniprésente même si l’on peut observer que la rive droite est légèrement plus cultivée de part son exposition au soleil sud/sud-est.
+    <br>    Ses sols riches en craie typique de la région convienne à merveille à la vigne et surtout au cépage du pinot noir.
+    <br>    Enfin il est important de noter que c’est dans cette vallée que l’on trouve Le Mémorial commémoratif des deux batailles de la Marne, importants épisodes de la première guerre mondiale.
+</p>
+
+
+"
+
+
+)
---
+++ pages_php/type.php
@@ -0,0 +1,45 @@
+<php? require(coteaux.php); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=devic-width, initial-scale=1,0">
+    <link rel="stylesheet" href="../css/style-type.css">
+    
+
+    <title>Type</title>
+</head>
+<body>
+
+    <div class="fond">
+
+    <div class="containertitre versionPC">
+        <a class='clicable' href="">Hébergement</a> 
+        <div class="trait_gris"></div>
+        <a class='clicable' href="">Découvrir</a>  
+        <div class="trait_gris"></div>
+        <a class='clicable' href="">Restaurant</a> 
+        <div class="trait_gris"></div>
+        <a class='clicable' href="">Agenda</a>
+    </div>
+
+    <div class="containertitre versionTel">
+        <a class='clicable' href="">Champagne</a>
+    </div>
+
+
+    <div class="containercorp">
+
+        <div class="text centre">
+            
+            <?= $article[1] ?>
+            
+        </div>
+        
+    </div>
+
+    <div class="containerfin"></div>
+</div>
+</body>
+</html>
