2393bff3174b24833567f28d0034b619eae194b0
Kilouhardi <Joshual.courriel@gmail.com>
2022-05-03 15:09:42 +0200

    mise en place des pages php
    On a maintenant des pages php avec un header et un footer.
    Les liens interpages sont fonctionnels.
    Manque encore la traduction

---
+++ pages_php/Coteaux.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=devic-width, initial-scale=1,0">
+    <link rel="stylesheet" href="../css/style-type.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    
+    <title>Coteaux</title>
+    <style>
+        #fond{
+            background-attachment: fixed;
+            background-position:center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-image: url(../image/Photo\ by\ Claudio\ Schwarz\ on\ Unsplash\(2\).jpg);
+            position: fixed;
+            width: 100vw;
+            height: 100vh;
+            filter:blur(2px);
+        }
+        </style>
+    
+</head>
+<body>
+<?php include 'header.php';?>
+        <div id="fond"></div>
+        
+        <div class="containercorp">
+            <div class="text centre">
+                <h2>Les coteaux de Champagne</h2>
+               <p>
+                &nbsp &nbsp &nbsp Depuis 2015, la région Champagne est inscrite au patrimoine mondial de l’UNESCO. Est inscrit à ce patrimoine ses caves, ses maisons et ses coteaux. En effet c’est bien grâce à sa géographie exceptionnelle que la Champagne a mérité sa place auprès d’autres lieux d'exceptions.
+                <br>&nbsp &nbsp &nbsp Pour tous ceux qui souhaitent voir les merveilles que propose cette région française, cet article est fait pour vous.
+                <br>
+                <br>
+                    Parmi tous les coteaux qui parcourent la vallée de la marne, trois ont été choisis pour représenter la région auprès du monde: les coteaux historiques de Hautvillers, Aÿ et Mareuil-sur-Aÿ. 
+                
+               </p>
+            </article>
+                <h3>La vallée de la Marne</h3>
+                <section>
+                <article>
+                <p>&nbsp &nbsp &nbspCette vallée qui s’étend de Tours-sur-Marne à Dormans traverse le cœur historique du vignoble champenois et possède en son sein une partie des coteaux les plus connus de la Champagne. On y trouve la culture de la vigne de manière omniprésente même si l’on peut observer que la rive droite est légèrement plus cultivée de part son exposition au soleil sud/sud-est
+                    <br>
+                    &nbsp &nbsp &nbspSes sols riches en craie typique de la région convienne à merveille à la vigne et surtout au cépage du pinot noir.
+                    <br>
+                    &nbsp &nbsp &nbspEnfin il est important de noter que c’est dans cette vallée que l’on trouve Le Mémorial commémoratif des deux batailles de la Marne, importants épisodes de la première guerre mondiale
+                </p>
+                    <img src="../image/Vallé de la Marne.jpg">
+                </article>
+                <br> 
+                <article>
+                    <h3>Les coteaux historiques de Hautvilliers</h3>
+                    <br> 
+                    <p>
+                    &nbsp &nbsp &nbsp Ces coteaux historiques qui entourent le village de Hautvillers est typique de la région champenoise. Ses sols crayeux surmontés d’une couche d'argile et de sable sont parfaits pour la culture de la vigne nécessaire à la boisson tant réputée qu’est le champagne. C’est d’ailleurs au cœur de ces coteaux que l’on trouve l’abbaye d’Hautvillers dans lequel le moine benedictin <a href="">Dom Pérignon</a> a vécu, lui qui a joué un rôle historique si important dans la naissance du champagne   
+                    </p>
+                    <img src = "../image/HautVillers.jpg">
+                    <br> 
+                </article>
+                <article>
+                    <h3>Les coteaux d'Aÿ</h3>
+                    <br> 
+                    <p>
+                   
+                        &nbsp &nbsp &nbspLes coteaux d’Aÿ sont des coteaux qui font face à la vallée de la Marne, c’est un vignoble très profond. Qui s’étend sur des zones boisées aussi bien que sur des plaines inondables, composé de pente raide et d'ondulations douces le long desquelles on trouve une quasi monoculture de vigne depuis des années, témoin du savoir faire de l’endroit.
+                    </p>
+                    <br> 
+                </article>
+                <article>
+                    <h3>Les coteaux de Mareuil-sur-Aÿ</h3>
+                    <br> 
+                    <p>
+                        &nbsp &nbsp &nbsp Au cœur de 25 km de caves sous la ville de Reims, près de 25 Millions de bouteilles sont entreposées, éclairées par un joli jeu de lumière. Ainsi Mumm abrite plus de deux siècles de savoir faire et de passion.
+                        Depuis la création de la cuvée “Cordon rouge” , Mumm est reconnue dans le monde entier comme une maison d'excellence. Pour l’histoire,  c’est en novembre 1876 que Georges Hermann Mumm (G.H.)  fait enregistrer la dénomination « Cordon Rouge » pour une partie de ses bouteilles. Sur la bouteille, ce ruban rouge apparenté à la Légion d’honneur française est devenu le signe distinctif d’excellence incarnant l’esprit de la Maison de champagne. 
+                    </p>
+                    <br> 
+                    <p> &nbsp &nbsp &nbsp Dominant la Marne à l’endroit où cette dernière rentre dans la “grande-vallé” ils offrent un magnifique panorama sur toute la région. De ces coteaux ont peut observer la ville d’Epernay ainsi que la vallée de la Marne qui portent en son sein les coteaux d’Hautvillers et d’Aÿ. 
+                        <br>Même si ils sont moins profonds que ceux d’Aÿ les coteaux de Mareuil-sur-Aÿ profitent d’une myriade d’exposition au soleil différente qui permet une diversité forte quant aux aspects de la vigne.
+                    </p>
+                    <br> 
+                </article>
+            </section>
+                </div>
+            </div>
+            
+            <div class= "sideicons "> 
+                <div class="iconimg"><a href=";"><img src="../image/bed.png"></a></div>
+                <div class="iconimg"><a href=";"><img  src="../image/restaurant.png"></a></div>
+                <div class="iconimg"><a href=";"><img  src="../image/event.png"></a></div>
+            </div>
+
+                <div class="container-gotop">
+                    <a href="#" class="gotop"><img class = "imggotop" src="../image/to_top.png"></a>
+                </div>
+        
+            </footer>
+        </body>
+        <?php include 'footer.php';?>
+        </html>
