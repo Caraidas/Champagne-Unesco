733e31bf1151f1f800b4aeef5310507fd0c3c7ef
Harulla <laura.leroychhon@gmail.com>
2022-05-11 10:10:01 +0200

    traduc caves

--- pages_php/Caves.php
+++ pages_php/Caves.php
@@ -1,3 +1,6 @@
+<?php include 'fonctionnels/langVerif.php';?>
+<?php include 'contenus/contenuArtisana.php';?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -29,94 +32,30 @@
         
         <div class="containercorp">
             <div class="text centre">
-               
-                <h2>Caves de Champagne</h2>
-                <p>Des premières crayères de Reims creusée entre le Xe et le XVIIIe siècle aux Galeries du XXe siècle, les caves de champagne constituent un élément phare du Patrimoine mondial de l’UNESCO. 
-                    <br>
-                    Le sol calcaire de champagne est idéal d'une part pour la culture de la vigne, mais l’est tout autant pour la conservation du Champagne assurant une température idéale pour la fermentation et la prise de mousse de ce merveilleux breuvage.
-                </p>
-                <p>Ici, retrouvez une sélection des plus belles caves de Champagne afin de vous immerger dans la fabrication du vin mousseux.</p>
-                <section>
-                <article>
-                <p>&nbsp &nbsp &nbspDes premières crayères de Reims creusée entre le Xe et le XVIIIe siècle aux Galeries du XXe siècle, les caves de champagne constituent un élément phare du Patrimoine mondial de l’UNESCO. 
-                    Le sol calcaire de champagne est idéal d'une part pour la culture de la vigne, mais l’est tout autant pour la conservation du Champagne assurant une température idéale pour la fermentation et la prise de mousse de ce merveilleux breuvage.
-                     
-                    Ici, retrouvez une sélection des plus belles caves de Champagne afin de vous immerger dans la fabrication du vin mousseux.
-                    
-                    </p>
-                </article>
-                <br> 
-                <article>
-                    <h3>Caves Joseph Perrier, Châlons-en-Champagne</h3>
-                    <br> 
-                    <img src = "../image/cave.jpg">
-                    <p>
-                    &nbsp &nbsp &nbsp Nous voici au cœur d’une cave de 3km de long datant du IVe siècle, 
-                    creusée dans d'anciennes crayères gallo-romaines. Depuis 1825, année de création de la Maison 
-                    Joseph Perrier au fondateur du même nom, les caves sont des éléments phare du paysage chalonnais.
-                     Ces caves de plain-pied ont été creusées dans la colline surplombant la Maison et sont protégées par un paysage arboré qui joue le rôle de régulateur climatique naturel permettant de conserver une hygrométrie et des températures optimales pour le vieillissement des vins.
-                    </p>
-                    <img src="../image/cavejoseph.png">
-                    <br> 
-                    <p>Les galeries sont percées par des trous laissant entrer des puits de lumière qu’on appelle essors. Ces essors permettaient avant tout de diriger la lumière pour le travail des ouvriers mais servent également de ventilation.
-                        En 2018, la maison décide de rénover ses caves afin de faciliter et rendre la visite de ses caves plus agréables et missionne le concepteur de lumière <a href="http://www.lumesens.com/">Emeric Thiénot</a> sur l’éclairage de ses caves.
-                        Afin d’éviter le “Goût Lumière” un défaut créé par une réaction chimique de la lumière sur le vin, Emeric Thiénot s’adapte à une contrainte d’un niveau très faible d’éclairement de 595nm et d’une couleur orangée rentrant en direct avec le vin le mettant en avant de la meilleur des façons. 
-                        Ainsi, ces caves abritent des millions de bouteilles de champagne vieillissant de 3 à 5 ans.
-                        </p>
-                </article>
-                <article>
-                    <h3>Caves de Champagne Moët et Chandon</h3>
-                    <br> 
-                    <img src = "../image/moetchandon2.jpg">
-                    <br> 
-                    <p>
-                   
-                        &nbsp &nbsp &nbsp Moët et Chandon,
-                        grande maison de champagne fondée en 1743 possède les plus grandes caves de Champagne creusée dans la craie champenoise.
-                        Avec 28 km de galeries s'étendant sur plusieurs niveaux sous la grande avenue de Champagne à Epernay ,
-                        les bouteilles de vin mousseux sont gardées dans la fraîcheur et l’humidité afin d’obtenir une maturation lente et unique.
-                    </p>
-                    <br> 
-                    <img src = "../image/moetchandon1.jpg">
-                    <br> 
-                    <p>Ces galeries interminables se coupent entre elles, créant un réel labyrinthe souterrain. 
-                        En marchant dans la pénombre de ces galeries , on peut observer les traces des outils qui ont taillé cette craie avec une faible lueur de lumière éclairant à peine les bouteilles  afin qu’elles gardent leur goût authentique.
-                    </p>
-                    <br> 
-                </article>
-                <article>
-                    <h3>Caves de Champagne Mumm</h3>
-                    <br> 
-                    <p>
-                        &nbsp &nbsp &nbsp Au cœur de 25 km de caves sous la ville de Reims, près de 25 Millions de bouteilles sont entreposées, éclairées par un joli jeu de lumière. Ainsi Mumm abrite plus de deux siècles de savoir faire et de passion.
-                        Depuis la création de la cuvée “Cordon rouge” , Mumm est reconnue dans le monde entier comme une maison d'excellence. Pour l’histoire,  c’est en novembre 1876 que Georges Hermann Mumm (G.H.)  fait enregistrer la dénomination « Cordon Rouge » pour une partie de ses bouteilles. Sur la bouteille, ce ruban rouge apparenté à la Légion d’honneur française est devenu le signe distinctif d’excellence incarnant l’esprit de la Maison de champagne. 
-                    </p>
-                    <br> 
-                    <img src = "../image/mumm.jpg">
-                    <br> 
-                    <p>Elaboré à partir d’un mariage de plusieurs crus provenant des grands terroirs de Champagne, le Cordon Rouge Mumm est composé en grande partie du Pinot Noir (45%) qui apporte structure et puissance, tandis que le Chardonnay (30%) apporte de la finesse et de l’élégance, enfin, Pinot Meunier son fruité et sa rondeur.
-                        Ainsi la cuvée cordon rouge est un champagne qui allie fruité et fraîcheur, parfait pour un apéritif. 
-                        La cuvée Mumm Grand Cordon quant à elle, fait honneur aux pinots noirs, issus des meilleurs crus. Ils s’accordent à la finesse et à l’élégance des chardonnays et à la rondeur fruitée des pinots meuniers.
-                    </p>
-                    <br>
-                </article>
-                <article>
-                <h3>Caves de Champagne Veuve Clicquot</h3>
-                <br> 
-                <p>
-                    &nbsp &nbsp &nbsp  En 1909, la Maison Veuve Clicquot Ponsardin acquiert des crayères à la périphérie du centre-ville de Reims. Ces carrières souterraines témoignent des différentes activités humaines qui les ont façonnées au fil des siècles. Les carriers, puis les cavistes, avaient pour tradition de laisser des marques de leur passage en y gravant des graffitis aux motifs divers. Les réfugiés civils et militaires de la Grande Guerre, eux aussi, ont confié leurs états d’âme aux parois de craie.
-                </p>
-                <br> 
+                <?= $titre[$langue] ?>
+
+                <?= $para1[$langue] ?>
+                <img src = "../../image/cave.jpg">
+                
+                <?= $para2[$langue] ?>
+                <img src="../image/cavejoseph.png">
+                <?= $para3[$langue] ?>
+
+                <img src = "../image/moetchandon2.jpg"> 
+                <? $para4[$langue] ?>
+                <img src = "../image/moetchandon1.jpg">
+                <? $para5[$langue] ?>
+
+                <? $para6[$langue] ?>
+                <img src = "../image/mumm.jpg">
+                <? $para7[$langue] ?>
                 <img src = "../image/veuveclicquot.jpg">
                 <br> 
-                <br>
                 <img src = "../image/veuveclicquot2.jpg">
-                <br>
+
                 <p>Dans ce labyrinthe situé à vingt mètres sous terre après cet escalier lumineux, la température des crayères, leur hygrométrie constante, ainsi que le calme qui y règne, en font le lieu idéal pour y accueillir la production de champagne.
                     Ainsi retrouve-t-on la cuvée emblématique “Le brut Carte Jaune” reflet du vignoble de la Maison et du terroir champenois mais aussi la cuvée en l’honneur de Madame Cliquot “La Grande dame”.
                 </p>
-                </article>
-            </section>
                 </div>
             </div>
 
