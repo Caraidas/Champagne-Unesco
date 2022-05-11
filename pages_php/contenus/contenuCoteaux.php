0cc8ba7fa727aae94c74c18e4e30ab9b96ccea83
Kilouhardi <Joshual.courriel@gmail.com>
2022-05-11 09:44:11 +0200

    coteaux traduis

---
+++ pages_php/contenus/contenuCoteaux.php
@@ -0,0 +1,73 @@
+<?php
+
+$titre = array(
+    "<h2>Les coteaux de Champagne</h2>",
+    "<h2>the hillsides of champagne</h2>"
+);
+
+$text1 = array(
+    "<p>
+        &nbsp &nbsp &nbsp Depuis 2015, la région Champagne est inscrite au patrimoine mondial de l’UNESCO. Est inscrit à ce patrimoine ses caves, ses maisons et ses coteaux. En effet c’est bien grâce à sa géographie exceptionnelle que la Champagne a mérité sa place auprès d’autres lieux d'exceptions.<br>
+        &nbsp &nbsp &nbsp Pour tous ceux qui souhaitent voir les merveilles que propose cette région française, cet article est fait pour vous.<br><br>
+        Parmi tous les coteaux qui parcourent la vallée de la marne, trois ont été choisis pour représenter la région auprès du monde: les coteaux historiques de Hautvillers, Aÿ et Mareuil-sur-Aÿ. 
+    </p>
+    <h3>La vallée de la Marne</h3>
+    <p>
+        &nbsp &nbsp &nbspCette vallée qui s’étend de Tours-sur-Marne à Dormans traverse le cœur historique du vignoble champenois et possède en son sein une partie des coteaux les plus connus de la Champagne. On y trouve la culture de la vigne de manière omniprésente même si l’on peut observer que la rive droite est légèrement plus cultivée de part son exposition au soleil sud/sud-est <br>
+        &nbsp &nbsp &nbspSes sols riches en craie typique de la région convienne à merveille à la vigne et surtout au cépage du pinot noir.<br>
+        &nbsp &nbsp &nbspEnfin il est important de noter que c’est dans cette vallée que l’on trouve Le Mémorial commémoratif des deux batailles de la Marne, importants épisodes de la première guerre mondiale
+    </p>",
+    "<p>
+        &nbsp &nbsp &nbsp Since 2015, the Champagne region has been listed as a UNESCO World Heritage Site. Its cellars, houses and hillsides are part of this heritage. Indeed, it is thanks to its exceptional geography that the Champagne region has earned its place alongside other exceptional places. <br>
+        &nbsp &nbsp &nbsp For all those who wish to see the wonders what this French region offers, this article is made for you.<br><br>
+        Of all the hillsides that run through the Marne Valley, three have been chosen to represent the region to the world: the historic hillsides of Hautvillers, Aÿ and Mareuil-sur-Aÿ.
+    </p>
+    <h3>The Marne Valley</h3>
+    <p>
+        &nbsp &nbsp &nbsp This valley, which stretches from Tours-sur-Marne to Dormans, runs through the historic heart of the Champagne vineyards and has some of the best-known hillsides of the Champagne region within it. Vines grow everywhere, although the right bank is slightly more cultivated due to its exposure to the south/south-east sun. <br>
+        &nbsp &nbsp &nbsp Its chalky soil, typical of the region, is perfectly suited to the vine and especially to the Pinot Noir grape variety.<br>
+        &nbsp &nbsp &nbsp Finally it is important to note that it is in this valley that we find the Memorial of the two battles of the Marne, important episodes of the First World War.
+    </p>"
+);
+
+$text2 = array(
+    "<h3>Les coteaux historiques de Hautvilliers</h3>             
+    <p>
+        &nbsp &nbsp &nbsp Ces coteaux historiques qui entourent le village de Hautvillers est typique de la région champenoise. Ses sols crayeux surmontés d’une couche d'argile et de sable sont parfaits pour la culture de la vigne nécessaire à la boisson tant réputée qu’est le champagne. C’est d’ailleurs au cœur de ces coteaux que l’on trouve l’abbaye d’Hautvillers dans lequel le moine benedictin <a href='HistoireCreationChampagne.php'>Dom Pérignon</a> a vécu, lui qui a joué un rôle historique si important dans la naissance du champagne   
+    </p>",
+    "<h3>The historic hillsides of Hautvillers</h3>
+    <p>
+        &nbsp &nbsp &nbsp These historic hillsides surrounding the village of Hautvillers are typical of the Champagne region. Its chalky soils, topped by a layer of clay and sand, are perfect for growing the vines needed to produce the famous drink that is champagne. It is in the heart of these hillsides that we find the abbey of Hautvillers in which the Benedictine monk <a href='HistoireCreationChampagne.php'>Dom Pérignon</a> lived, he played such an important historical role in the birth of champagne
+    </p>
+    "
+);
+
+$text3 = array(
+    "<h3>Les coteaux d'Aÿ</h3> 
+    <p>
+        &nbsp &nbsp &nbsp Les coteaux d’Aÿ sont des coteaux qui font face à la vallée de la Marne, c’est un vignoble très profond. Qui s’étend sur des zones boisées aussi bien que sur des plaines inondables, composé de pente raide et d'ondulations douces le long desquelles on trouve une quasi monoculture de vigne depuis des années, témoin du savoir faire de l’endroit.
+    </p>                
+    <h3>Les coteaux de Mareuil-sur-Aÿ</h3> 
+    <p>
+        &nbsp &nbsp &nbsp Au cœur de 25 km de caves sous la ville de Reims, près de 25 Millions de bouteilles sont entreposées, éclairées par un joli jeu de lumière. Ainsi Mumm abrite plus de deux siècles de savoir faire et de passion.
+        Depuis la création de la cuvée “Cordon rouge” , Mumm est reconnue dans le monde entier comme une maison d'excellence. Pour l’histoire,  c’est en novembre 1876 que Georges Hermann Mumm (G.H.)  fait enregistrer la dénomination « Cordon Rouge » pour une partie de ses bouteilles. Sur la bouteille, ce ruban rouge apparenté à la Légion d’honneur française est devenu le signe distinctif d’excellence incarnant l’esprit de la Maison de champagne. 
+    </p> 
+    <p> 
+        &nbsp &nbsp &nbsp Dominant la Marne à l’endroit où cette dernière rentre dans la 'grande-vallé' ils offrent un magnifique panorama sur toute la région. De ces coteaux ont peut observer la ville d’Epernay ainsi que la vallée de la Marne qui portent en son sein les coteaux d’Hautvillers et d’Aÿ.<br> 
+        Même si ils sont moins profonds que ceux d’Aÿ les coteaux de Mareuil-sur-Aÿ profitent d’une myriade d’exposition au soleil différente qui permet une diversité forte quant aux aspects de la vigne.
+    </p>",
+    "<h3>The hillsides of Aÿ</h3>
+    <p>
+        &nbsp &nbsp &nbsp The slopes of Aÿ are hillsides that face the Marne valley, it is a very deep vineyard. It extends over wooded areas as well as flood plains, composed of steep slopes and gentle undulations along which there has been an almost monoculture of vines for many years, a testimony to the know-how of the place.
+    </p>
+    <h3>The hillsides of Mareuil-sur-Aÿ</h3>
+    <p>
+        &nbsp &nbsp &nbsp
+    </p>
+    <p>
+        &nbsp &nbsp &nbsp Overlooking the Marne at the point where it enters the 'Grande Vallée', they offer a magnificent panorama of the whole region. From these hillsides you can see the town of Epernay as well as the Marne valley, which contains the hillsides of Hautvillers and Aÿ.
+        Even if they are less deep than those of Aÿ, the slopes of Mareuil-sur-Aÿ benefit from a myriad of different sun exposures which allow a strong diversity in the aspects of the vine.        
+    </p>
+    
+    "
+);
