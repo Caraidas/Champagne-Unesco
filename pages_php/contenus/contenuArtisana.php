6d84f5efc36b1b18434cc2d61d3d361edde924cc
Kilouhardi <Joshual.courriel@gmail.com>
2022-05-11 08:51:56 +0200

    opti des fichiers

---
+++ pages_php/contenus/contenuArtisana.php
@@ -0,0 +1,21 @@
+<?php
+
+$titre = array(
+    "<h2>L'artisanat et les activités de Champagne</h2>",
+    "<h2>Champagne crafts and activities</h2>"
+);
+
+$para1 = array(
+    "<p>&nbsp &nbsp &nbsp La ville de Reims est une ville extrêmement vivante et fière de son patrimoine. En tant que tel elle vous proposera de découvrir tout ce qui fait le charme de la Champagne et de vivre des expériences inoubliables. Vous avez toujours voulu partager la journée d’un vigneron? Vous voulez découvrir le savoir-faire d’un vrai tonnelier?  Ou peut-être que parcourir les vignes pour récolter le raisin lors d’une belle journée vous intéresse? <br>N’attendez plus et venez vivre une de ces <a href='https://www.reims-tourisme.com/experiences'>expériences rémoises!</a></p>",
+    "<p>&nbsp &nbsp &nbsp The city of Reims is an extremely lively city and proud of its heritage. As such, it will offer you the opportunity to discover what makes the Champagne region so charming and to live unforgettable experiences. Have you always wanted to share a winemaker's day? Would you like to discover the know-how of a real cooper?  Or maybe you are interested in going through the vineyards to harvest the grapes on a beautiful day? Don't wait any longer and come and enjoy one of <a href='https://www.reims-tourisme.com/experiences'>these experiences</a> in this beautiful region!</p>"
+);
+
+$para2 = array(
+    "<p>&nbsp &nbsp &nbsp Mais il n’y à pas que la ville de Reims qui vous propose de vivre des expériences inattendues et de découvrir le savoir faire des artisans de  la région. La Marne tout entière regorge d'artisans talentueux ayant un vrai plaisir à vous faire découvrir leur travail. Que ce soit un Maître verrier, un tourneur sur bois ou une potière passionnée qui pourrait vous introduire à son monde, il est certain que vous trouverez de quoi émerveiller les petits mais aussi les plus grands.</p>",
+    "<p>&nbsp &nbsp &nbsp But it is not only the city of Reims that offers you unexpected experiences and the opportunity to discover the know-how of the region's craftsmen. The whole of the Marne region is full of talented craftsmen who take great pleasure in showing you their work. Whether it's a master glassmaker, a woodturner or a passionate potter who could introduce you to her world, you're sure to find something to amaze the little ones as well as the older ones.</p>"
+);
+
+$para3 = array(
+    "<p>&nbsp &nbsp &nbsp Enfin peut être que vous êtes plus intéressé par une après midi shopping que par la découverte du métier d'artisan. Ici encore le savoir-faire à sa place. La champagne à du goût et propose un grand nombre de produits d'artisanat varié. Laissez vous guider par <a href='https://lachampagneadugout.com/artisanat-et-culture/artisans-createurs/'>ici</a>, vous ne le regretterez pas !</p>",
+    "<p>&nbsp &nbsp &nbsp Perhaps you are more interested in an afternoon of shopping than in discovering the craft. Here again, know-how has its place. Champagne has taste and offers a large number of varied handicraft products. Let yourself be guided <a href='https://lachampagneadugout.com/artisanat-et-culture/artisans-createurs/'>here</a>, you won’t regret it:</p>"
+);
