b22fb724675852b0867dd291fef3d4aeaf98da32
azahar03 <96001060+azahar03@users.noreply.github.com>
2022-06-11 16:22:12 +0200

    Traduction fort et degustation

--- pages_php/FortChabrol.php
+++ pages_php/FortChabrol.php
@@ -29,43 +29,21 @@
     
         <div class="containercorp">
             <div class="text centre">
-               
-                
-                
-                    <h2>Fort Chabrol</h2>
-                
-                <p>&nbsp &nbsp &nbspLa Les caves, côteaux et maisons de champagne ne sont pas les seuls éléments inscrits sur la liste du patrimoine mondial de l’UNESCO. En effet, voici un autre , un bâtiment érigé à la fin du XIXème siècle dans la ville d’Epernay: le Fort Chabrol.
-                
-                </p>
-                    <img src="../image/fortChabrol.jpg">
-                
-                
-                    <br>
-                    <p>
-                        Achevé en 1900, le bâtiment accueillit l'école pratique de viticulture de la grande maison Moët & Chandon où furent formées des générations de jeunes Champenois. Les "brevets de Viticulture champenoise" y étaient encore décernés au début des années 1980.
-                    </p>
-                    <br>
-                    <p>Avec  son laboratoire de recherches en viticulture et œnologie, la Maison et son "fort" joueront un rôle déterminant dans la lutte contre le phylloxéra. Le <b>phylloxéra de la vigne</b> est une sorte de pucerons ravageurs de la vigne. Le terme de phylloxéra désigne ainsi par métonymie, la maladie de la vigne causée par cet insecte.
-                        Les premières traces en sont repérées en Champagne le 6 août 1890 à Tréloup-Chassaing (Aisne), puis dans la Marne, à Mardeuil et au Mesnil-sur-Oger, en 1892.
-                    </p>
-                    
-                    <img src = "../image/phylloxera.jpg">
-                
-                    <br>
-                    <p>Raoul Chandon de Briailles commença par racheter, arracher et brûler la première parcelle atteinte. Les Champenois purent en outre bénéficier du résultat des longues recherches déjà menées, entre autres lieux, à Bordeaux et Montpellier. Sulfure de carbone et sulfocarbonate de potassium représentent, dans un premier temps, les seuls moyens efficaces avant que le  fort, mette en place un station de greffage, permettant d’aboutir au greffage sur des porte-greffes américains, résistants au phylloxéra, de cépages champenois qui conservaient toutes leurs caractéristiques. Ainsi, des millions de greffes issues des ateliers créés par Raoul Chandon de Briailles permirent de replanter 115 ha de 1898 à 1911.
-                    </p>
-                    <img src = "../image/RaoulChandon.jpg">
-                    <br>
-                    <p>
-                        A ce titre, le Fort Chabrol fut le témoignage de l’œuvre collective de lutte contre le phylloxera, où se matérialise une solidarité des viticulteurs champenois dans la lutte contre l’insecte.
-                    </p>
-            
-                </div>
-       
-            </div>
-        <?php include 'footer.php';?>
-</body>
+            <?= $titre[$langue] ?>
 
+            <?= $para1[$langue] ?>
+            <img src="../image/fortChabrol.jpg">
+
+            <?= $para2[$langue] ?>
+            <img src = "../image/phylloxera.jpg">
+                
+            <?= $para3[$langue] ?>
+            <img src = "../image/RaoulChandon.jpg">
+            
+            <?= $para5[$langue] ?>
+                </div>
+            </div>
+            </body>
+            <?php include 'footer.php';?>
 </html>
-    </body>
-    </html>
+    
---
+++ pages_php/contenus/contenurFort.php
@@ -0,0 +1,26 @@
+<?php
+
+$titre = array(
+    "<h2>Fort Chabrol</h2>",
+    "<h2>Fort Chabrol</h2>"
+);
+
+$para1 = array(
+    "<p>&nbsp &nbsp &nbspLa Les caves, côteaux et maisons de champagne ne sont pas les seuls éléments inscrits sur la liste du patrimoine mondial de l’UNESCO. En effet, voici un autre , un bâtiment érigé à la fin du XIXème siècle dans la ville d’Epernay: le Fort Chabrol.</p>"
+    "<p>&nbsp &nbsp &nbspThe cellars, vineyards and champagne houses are not the only elements inscribed on the UNESCO World Heritage list. Indeed, here's another, a building erected at the end of the 19th century in the town of Epernay: Fort Chabrol.</p>"
+);
+
+$para2 = array(
+    "<br><p>Achevé en 1900, le bâtiment accueillit l'école pratique de viticulture de la grande maison Moët & Chandon où furent formées des générations de jeunes Champenois. Les "brevets de Viticulture champenoise" y étaient encore décernés au début des années 1980.</p><br><p>Avec son laboratoire de recherches en viticulture et œnologie, la Maison et son "fort" joueront un rôle déterminant dans la lutte contre le phylloxéra. Le <b>phylloxéra de la vigne</b> est une sorte de pucerons ravageurs de la vigne. Le terme de phylloxéra désigne ainsi par métonymie, la maladie de la vigne causée par cet insecte.Les premières traces en sont repérées en Champagne le 6 août 1890 à Tréloup-Chassaing (Aisne), puis dans la Marne, à Mardeuil et au Mesnil-sur-Oger, en 1892.</p>"
+    "<br><p>Completed in 1900, the building housed the practical school of viticulture of the great house Moët & Chandon where generations of young Champenois were trained. The "Viticulture champenoise patents" were still awarded there in the early 1980s.</p><br><p>With its research laboratory in viticulture and oenology, the House and its "fort" will play a determining role in the fight against phylloxera. <b>Phylloxera</b> is a type of vine aphid pest. The term phylloxera is used metonymically to designate the vine disease caused by this insect. The first traces of it were spotted in Champagne on August 6, 1890 in Tréloup-Chassaing (Aisne), then in the Marne, in Mardeuil and Mesnil-sur-Oger, in 1892.</p>"
+);
+
+$para3 = array(
+    "<br><p>Raoul Chandon de Briailles commença par racheter, arracher et brûler la première parcelle atteinte. Les Champenois purent en outre bénéficier du résultat des longues recherches déjà menées, entre autres lieux, à Bordeaux et Montpellier. Sulfure de carbone et sulfocarbonate de potassium représentent, dans un premier temps, les seuls moyens efficaces avant que le  fort, mette en place un station de greffage, permettant d’aboutir au greffage sur des porte-greffes américains, résistants au phylloxéra, de cépages champenois qui conservaient toutes leurs caractéristiques. Ainsi, des millions de greffes issues des ateliers créés par Raoul Chandon de Briailles permirent de replanter 115 ha de 1898 à 1911.</p>",
+    "<br><p>Raoul Chandon de Briailles began by buying back, uprooting and burning the first affected plot. The Champenois were also able to benefit from the results of the long research already carried out, among other places, in Bordeaux and Montpellier. Carbon sulphide and potassium sulphocarbonate were initially the only effective means before the fort set up a grafting station, enabling the grafting of Champagne grape varieties that retained all their characteristics onto American rootstocks that were resistant to phylloxera. Thus, millions of grafts from the workshops created by Raoul Chandon de Briailles made it possible to replant 115 ha from 1898 to 1911.</p>"
+);
+
+$para4 = array(
+    "<br><p>A ce titre, le Fort Chabrol fut le témoignage de l’œuvre collective de lutte contre le phylloxera, où se matérialise une solidarité des viticulteurs champenois dans la lutte contre l’insecte.</p>"
+    "<br><p>In this respect, Fort Chabrol was a testimony to the collective work of the fight against phylloxera, where the solidarity of the Champagne winegrowers in the fight against the insect materialized</p>."
+)
