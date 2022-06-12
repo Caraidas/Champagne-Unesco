58a415474f052641e61a92605037a2874f5adbde
Kilouhardi <Joshual.courriel@gmail.com>
2022-06-12 21:54:29 +0200

    micro push

--- pages_php/contenus/contenuPatrimoine.php
+++ pages_php/contenus/contenuPatrimoine.php
@@ -7,24 +7,24 @@ $titre = array(
 
 $para1 = array(
     "<h3>Qu'est-que UNESCO?</h3>
-    <p>UNESCO, Organisation des Nations unies pour l'éducation, la science et la culture, est une institution spécialisée internationale de l'Organisation des Nations unies (ONU), créée le 16 novembre 1945 à la suite des dégâts et des massacres de la Seconde Guerre mondiale. Elle a pour objectif de contribuer au maintien de la paix et de la sécurité en resserrant, par l’éducation, la science et la culture, la collaboration entre nations afin d’assurer le respect universel de la justice, de la loi, des droits de l'Homme et des libertés fondamentales pour tous.
+    <p>&nbsp &nbsp &nbsp UNESCO, Organisation des Nations unies pour l'éducation, la science et la culture, est une institution spécialisée internationale de l'Organisation des Nations unies (ONU), créée le 16 novembre 1945 à la suite des dégâts et des massacres de la Seconde Guerre mondiale. Elle a pour objectif de contribuer au maintien de la paix et de la sécurité en resserrant, par l’éducation, la science et la culture, la collaboration entre nations afin d’assurer le respect universel de la justice, de la loi, des droits de l'Homme et des libertés fondamentales pour tous.
     UNESCO regroupe des sites exceptionnels éparpillés dans le monde entier.</p>",
 
     "<h3>What is UNESCO?</h3>
-    <p>UNESCO, United Nations Educational, Scientific and Cultural Organization, is a specialized international agency of the United Nations (UN), created on November 16, 1945 following the devastation and massacres of World War II. Its purpose is to contribute to peace and security by promoting collaboration among nations through education, science and culture in order to further universal respect for justice, for the rule of law and for human rights and fundamental freedoms.
+    <p>&nbsp &nbsp &nbsp UNESCO, United Nations Educational, Scientific and Cultural Organization, is a specialized international agency of the United Nations (UN), created on November 16, 1945 following the devastation and massacres of World War II. Its purpose is to contribute to peace and security by promoting collaboration among nations through education, science and culture in order to further universal respect for justice, for the rule of law and for human rights and fundamental freedoms.
     UNESCO brings together exceptional sites around the world.</p>"
 );
 
 
 $para2 = array(
     "<h3>Le bien inscrit à l'UNESCO</h3>
-    <p>Depuis le 4 juillet 2015, Les Côteaux, Maisons et Caves de Champagne sont inscrits sur la liste du patrimoine mondial, regroupant les sites les plus prestigieux du monde.</p>
-    <p>La catégorie visée par ce patrimoine désigne un ouvrage combiné de l’Homme et de la Nature, à savoir comment une activité humaine est la source de nouveaux paysages et d’une culture particulière.
+    <p>&nbsp &nbsp &nbsp Depuis le 4 juillet 2015, Les Côteaux, Maisons et Caves de Champagne sont inscrits sur la liste du patrimoine mondial, regroupant les sites les plus prestigieux du monde.</p>
+    <p>&nbsp &nbsp &nbsp La catégorie visée par ce patrimoine désigne un ouvrage combiné de l’Homme et de la Nature, à savoir comment une activité humaine est la source de nouveaux paysages et d’une culture particulière.
     Ainsi « Les Coteaux, Maisons et Caves de Champagne » sont présentés comme le paysage culturel qui a donné naissance au vin de Champagne aujourd'hui tourné vers l’excellence au niveau mondial.",
 
     "<h3>The property registered at UNESCO</h3>
-    <p>Since July 4, 2015, \"Côteaux, Maisons et Caves de Champagne\" have been included on the World Heritage List, grouping the most prestigious sites in the world.</p>
-    <p>The category covered by this heritage refers to a combined work of Man and Nature, in fact how a human activity is the source of new landscapes and a particular culture.
+    <p>&nbsp &nbsp &nbsp Since July 4, 2015, \"Côteaux, Maisons et Caves de Champagne\" have been included on the World Heritage List, grouping the most prestigious sites in the world.</p>
+    <p>&nbsp &nbsp &nbsp The category covered by this heritage refers to a combined work of Man and Nature, in fact how a human activity is the source of new landscapes and a particular culture.
     Thus \"Coteaux, Maisons et Caves de Champagne\" are presented as the cultural landscape that gave birth to the wine of Champagne today turned to excellence at the global level.
     "
 );
@@ -32,14 +32,14 @@ $para2 = array(
 
 
 $para3 =  array(
-   "<p>Pour figurer sur la Liste du patrimoine mondial, il faut satisfaire à au moins 1 des 10 critères de sélection définis par UNESCO. 
+   "<p>&nbsp &nbsp &nbsp Pour figurer sur la Liste du patrimoine mondial, il faut satisfaire à au moins 1 des 10 critères de sélection définis par UNESCO. 
         Pour les Coteaux, Maisons et Caves de Champagne, 3 de ces critères ont été reconnus :
     <br>
     -Le critère (iii), comme étant le résultat de savoir-faire perfectionnés au cours des générations, d’une organisation interprofessionnelle exemplaire et d’une protection de l’appellation, ainsi que du développement séculaire de relations interculturelles et d’innovations sociales.<br>
     -Le critère (iv), qui témoigne d’une pratique viticole et vinicole perfectionnée au cours des siècles et reposant sur un bassin d’approvisionnement (le vignoble), des lieux d’élaboration (les vendangeoirs, les caves…), de commercialisation et de distribution (les Maisons), ces divers éléments étant fonctionnellement imbriqués et intrinsèquement liés au substrat (la craie) support de la vigne, facile à creuser et que l’on retrouve dans l’architecture.<br>
     -Le critère (vi), reflet d’image symbolique unique au monde, référence universelle en termes de notoriété et de prestige, portée par les arts.</p>",
     
-    "<p>To be included on the World Heritage List, at least 1 of the 10 selection criteria defined by UNESCO must be met. 
+    "<p>&nbsp &nbsp &nbsp To be included on the World Heritage List, at least 1 of the 10 selection criteria defined by UNESCO must be met. 
     For the Coteaux, Maisons et Caves de Champagne, 3 of these criteria have been recognized:
     <br>  
     -(iii), as the result of know-how perfected over generations, exemplary interprofessional organization and protection of the appellation, as well as the age-old development of intercultural relations and social innovations.<br>
@@ -50,9 +50,9 @@ $para3 =  array(
 
 
 $para4 = array(
-    "<p>Les Coteaux, Maisons et Caves de Champagne sont composés de trois sites cœur dans le département de la Marne : la Colline Saint-Nicaise à Reims, les Coteaux de Cumières à Mareuil-sur-Aÿ, l’Avenue de Champagne à Epernay. Chacunes des trois catégories représente une étape essentielle du processus d’élaboration du champagne : la culture du raisin sur les coteaux, la fabrication du champagne et son vieillissement dans les caves ainsi que sa distribution dans le monde entier par l’intermédiaire des célèbres maisons de champagne.
+    "<p>&nbsp &nbsp &nbsp Les Coteaux, Maisons et Caves de Champagne sont composés de trois sites cœur dans le département de la Marne : la Colline Saint-Nicaise à Reims, les Coteaux de Cumières à Mareuil-sur-Aÿ, l’Avenue de Champagne à Epernay. Chacunes des trois catégories représente une étape essentielle du processus d’élaboration du champagne : la culture du raisin sur les coteaux, la fabrication du champagne et son vieillissement dans les caves ainsi que sa distribution dans le monde entier par l’intermédiaire des célèbres maisons de champagne.
     C’est l’assemblage de ces trois composantes, rurale, urbaine, souterraine, transformées par la culture du Champagne, qui donne son originalité et sa crédibilité à la candidature. A travers ces 3 sites témoins, c’est bien la totalité de la zone AOC Champagne qui est concernée par l’inscription.</p>",
-    "<p>\"Coteaux, Maisons et Caves de Champagne\" are composed of three core sites in the Marne department: the Colline Saint-Nicaise in Reims, the Coteaux de Cumières in Mareuil-sur-Aÿ, the Avenue de Champagne in Epernay.Each of the three categories represents an essential step in the champagne making process: the cultivation of the grapes on the hillsides, the making of the champagne and its ageing in the cellars, as well as its distribution around the world through the famous champagne houses. 
+    "<p>&nbsp &nbsp &nbsp \"Coteaux, Maisons et Caves de Champagne\" are composed of three core sites in the Marne department: the Colline Saint-Nicaise in Reims, the Coteaux de Cumières in Mareuil-sur-Aÿ, the Avenue de Champagne in Epernay.Each of the three categories represents an essential step in the champagne making process: the cultivation of the grapes on the hillsides, the making of the champagne and its ageing in the cellars, as well as its distribution around the world through the famous champagne houses. 
     It is the assembly of these three components, rural, urban, underground, transformed by the culture of Champagne, which gives its originality and its credibility to the candidature. Through these 3 witness sites, it is indeed the whole AOC Champagne area which is concerned by the inscription.</p>"
 );
 
