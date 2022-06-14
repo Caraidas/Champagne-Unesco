c35b38c1c3c42e422cf80ae74c285e2bc057181f
Kilouhardi <Joshual.courriel@gmail.com>
2022-06-14 17:04:04 +0200

    traduction quizz

--- pages_php/contenus/contenuQuizz.php
+++ pages_php/contenus/contenuQuizz.php
@@ -1,7 +1,7 @@
 <?php
 
 $question1 = array(
-    "<li class = <?=$color?>>
+    "
         <h3>Quel est le chef-lieu de la Champagne?</h3>
         
         <div>
@@ -25,8 +25,8 @@ $question1 = array(
         </div>
 
 
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
     <h3>What is the main town of the Champagne region?</h3>
     
         <div>
@@ -50,12 +50,12 @@ $question1 = array(
         </div>
 
 
-    </li>"
+    "
 
 );
 
 $question2 = array(
-    "<li class = <?=$color?>>
+    "
                 
         <h3>Le champagne (boisson) est :</h3>
         
@@ -79,8 +79,8 @@ $question2 = array(
             <label for='question-2-answers-D'>D) Juste un vin qui fait des bulles</label>
         </div>
 
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
                 
         <h3>Champagne (drink) is:</h3>
         
@@ -104,12 +104,12 @@ $question2 = array(
             <label for='question-2-answers-D'>D) just a wine that makes bubbles</label>
         </div>
 
-    </li>"
+    "
     
 );
 
 $question3=array(
-    "<li class = <?=$color?>>
+    "
                 
         <h3>La champagne est elle une grande région?</h3>
         
@@ -128,8 +128,8 @@ $question3=array(
             <label for='question-3-answers-C'>C) Non (+1 million d'habitants)</label>
         </div>
 
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
                 
         <h3>Is Champagne a large region?</h3>
         
@@ -148,12 +148,12 @@ $question3=array(
             <label for='question-3-answers-C'>C) No (+1 million inhabitants)</label>
         </div>
 
-    </li>"
+    "
     
 );
 
 $question4=array(
-    "<li class = <?=$color?>>
+    "
                 
         <h3>La champagne est situé:</h3>
         
@@ -177,8 +177,8 @@ $question4=array(
             <label for='question-4-answers-D'>D)  <img src='../image/cartequiz4.png'></label>
         </div>
 
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
                 
         <h3>The champagne is located:</h3>
         
@@ -202,11 +202,11 @@ $question4=array(
             <label for='question-4-answers-D'>D)  <img src='../image/cartequiz4.png'></label>
         </div>
 
-    </li>"
+    "
 );
 
 $question5=array(
-    "<li class = <?=$color?>>
+    "
                 
         <h3>Les couleurs  de la région Champagne-Ardennes sont:</h3>
         
@@ -230,8 +230,8 @@ $question5=array(
             <label for='question-5-answers-D'>D) Rouge Jaune</label>
         </div>
 
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
                 
         <h3>The colours of the Champagne-Ardennes region are</h3>
         
@@ -255,11 +255,11 @@ $question5=array(
             <label for='question-5-answers-D'>D) Red Yellow</label>
         </div>
 
-    </li>"
+    "
 );
 
 $question6=array(
-    "<li class = <?=$color?>>
+    "
         
         <h3>La Champagne est reconnu patrimoine mondiale de l’Unesco:</h3>
         
@@ -273,8 +273,8 @@ $question6=array(
             <label for='question-6-answers-B'>B) Faux</label>
         </div>
     
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
         
         <h3>Champagne is a UNESCO World Heritage Site:</h3>
         
@@ -288,11 +288,11 @@ $question6=array(
             <label for='question-6-answers-B'>B) False</label>
         </div>
     
-    </li>"
+    "
 );
 
 $question7=array(
-    "<li class = <?=$color?>>
+    "
             
         <h3>Combien de bouteilles de Champagne ont été produites en 2020?</h3>
         
@@ -311,8 +311,8 @@ $question7=array(
             <label for='question-7-answers-C'>C) 231 millions</label>
         </div>
 
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
             
         <h3>How many bottles of champagne were produced in 2020?</h3>
         
@@ -331,11 +331,11 @@ $question7=array(
             <label for='question-7-answers-C'>C) 231 millions</label>
         </div>
 
-    </li>"
+    "
 );
 
 $question8=array(
-    "<li class = <?=$color?>>
+    "
             
         <h3>La surface de vigne représente approximativement:</h3>
         
@@ -359,8 +359,8 @@ $question8=array(
             <label for='question-8-answers-D'>D) 58 hectares</label>
         </div>
 
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
             
         <h3>The vineyard area represents approximately:</h3>
         
@@ -384,11 +384,11 @@ $question8=array(
             <label for='question-8-answers-D'>D) 58 hectares</label>
         </div>
 
-    </li>"
+    "
 );
 
 $question9=array(
-    "<li class = <?=$color?>>
+    "
             
         <h3>Le pape Urbain II (qui lança les premières croisades) est née en Champagne:</h3>
         
@@ -402,8 +402,8 @@ $question9=array(
             <label for='question-9-answers-B'>B) Faux</label>
         </div>
 
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
             
         <h3>Pope Urban II (who launched the first crusades) was born in Champagne:</h3>
         
@@ -417,11 +417,11 @@ $question9=array(
             <label for='question-9-answers-B'>B) False</label>
         </div>
 
-    </li>"
+    "
 ),
 
 $question10=array(
-    "<li class = <?=$color?>>
+    "
         <h3>Quels événements ont lieu en Champagne:</h3>
         
         <div>
@@ -443,8 +443,8 @@ $question10=array(
             <input type='checkbox' name='question-10-answers[]' id='question-10-answers-D' value='D' required/>
             <label for='question-10-answers-D'>D) Festival de l'Ivresse</label>
         </div>
-    </li>",
-    "<li class = <?=$color?>>
+    ",
+    "
         <h3>What events take place in Champagne?</h3>
         
         <div>
@@ -466,6 +466,76 @@ $question10=array(
             <input type='checkbox' name='question-10-answers[]' id='question-10-answers-D' value='D' required/>
             <label for='question-10-answers-D'>D) Festival of Drunkenness</label>
         </div>
-    </li>"
+    "
 
-),
+);
+
+$para1 = array("<p>réponse: Chalon-En-Champagne</p>
+<p>C’est à Chalon-en-Champagne que l’on trouve la préfecture, c’est donc le chef-lieu de la Champagne.</p>
+",
+"<p>answer: Chalon-En-Champagne</p>
+<p>Chalon-en-Champagne is home to the prefecture, so it’s  the main townl of the Champagne region.</p>
+");
+
+$para2 = array("<p>réponse: une AOC</p>
+<p>A la différence de l’AOP qui est une appellation européenne, l'AOC est francais. Cette appellation permet de protéger la tradition française et évite que n’importe qui puisse appeler son vin pétillant du Champagne. </p>
+",
+"<p>answer: an AOC</p>
+<p>Unlike the AOP which is a European appellation, the AOC is French. This appellation protects the French tradition and prevents anyone from calling their sparkling wine Champagne.</p>
+");
+
+$para3 = array("<p>réponse: Non (+1 million d’habitants)</p>
+<p>Et non, le champagne n’est pas une région très peuplée. Pas très étonnant lorsque l’on connaît la place que prennent les vignes champenoises. </p>
+",
+"<p>answer: No (+1 million inhabitants)</p>
+<p>And no, Champagne is not a very populated region. Not very surprising when you know how much space the Champagne vineyards take up.</p>
+");
+
+$para4 = array("<p>réponse: Vrai</p>
+<p>Et oui La région est mondialement reconnue, elle fut intégrée en 2015. Pour plus de détail un article patrimoine existe sur notre site-web.</p>
+",
+"<p>answer: True</p>
+<p>Yes, the region is world-renowned and was included in 2015. For more details a heritage article exists on our website.</p>
+");
+
+$para5 = array("<p>réponse: Nord-Est</p>
+<p>Cette belle région se trouve à 120 km de Paris. Elle s’étend de Maux jusqu'à Epernay et empiète sur la Marne, l’Aube  et l’Aisne</p>
+",
+"<p>answer: North East</p>
+<p>This beautiful region is 120 km from Paris. It stretches from Maux to Epernay and encroaches on the Marne, Aube and Aisne</p>
+");
+
+$para6 = array("<p>réponses : Marché de Noël / Fêtes Johanniques.</p>
+<p>Comme partout, il y a de grands marchés de Noël en Champagne. MAis le plus intéressant sont les fêtes johanniques qui rendent hommage à Jeanne d’Arc et qui fêtent le sacre de Charle VII à Reims, une des plus grandes villes de la Champagne.</p>
+",
+"<p>answers: Christmas Market / Johannine Festivals.</p>
+<p>Like everywhere, there are great Christmas markets in Champagne. But the most interesting are the Johannine festivals which pay tribute to Joan of Arc and celebrate the coronation of Charles VII in Reims, one of the largest cities in Champagne.</p>
+");
+
+$para7 = array("<p>réponse: Bleu Or Blanc</p>
+<p>Ces couleurs représentent bien la Champagne n’est-ce pas? Les deux bandes dorées qu’on appellent des potences symbolisent toutes les châtellenies qui ont constitué le comté de Champagne.</p>
+",
+"<p>answer: Blue Gold White</p>
+<p>These colours represent Champagne, don't they? The two golden stripes, which are called “potence”, symbolise all the castellanies that made up the county of Champagne.</p>
+");
+
+$para8 = array("<p>réponse: 231 millions</p>
+<p>Ce nombre impressionnant montre bien la dévotion des artisans champenois à leur beau métier.</p>
+",
+"<p>answer: 231 million</p>
+<p>This impressive number shows the devotion of the Champagne craftsmen to their beautiful profession.</p>
+");
+
+$para9 = array("<p>réponse:34 000 hectares</p>
+<p>Soit 84 000 terrains de foot. C’est beaucoup.</p>
+",
+"<p>answer:34,000 hectares</p>
+<p>That's 84,000 football fields. That's a lot of land.</p>
+");
+
+$para10 = array("<p>réponse: Vrai</p>
+<p>Et oui la pape Urbain II est née en 1042 à Lagery, commune Champenoise.</p>
+",
+"<p>answer: True</p>
+<p>Yes, Pope Urban II was born in 1042 in Lagery, a commune in the Champagne region.</p>
+");
--- pages_php/quizgeneral.php
+++ pages_php/quizgeneral.php
@@ -1,4 +1,5 @@
 <?php include 'fonctionnels/langVerif.php';?>
+<?php include 'contenus/contenuQuiz.php';?>
 
 <!DOCTYPE html>
 <head>
@@ -25,84 +26,16 @@
             && isset($_POST['question-9-answers']) 
             && isset($_POST['question-10-answers'])){
                 $answer1 = $_POST['question-1-answers'];
-                $para1 = array("<p>réponse: Chalon-En-Champagne</p>
-                <p>C’est à Chalon-en-Champagne que l’on trouve la préfecture, c’est donc le chef-lieu de la Champagne.</p>
-                ",
-                "<p>answer: Chalon-En-Champagne</p>
-                <p>Chalon-en-Champagne is home to the prefecture, so it’s  the main townl of the Champagne region.</p>
-                ");
-
                 $answer2 = $_POST['question-2-answers'];
-                $para2 = array("<p>réponse: une AOC</p>
-                <p>A la différence de l’AOP qui est une appellation européenne, l'AOC est francais. Cette appellation permet de protéger la tradition française et évite que n’importe qui puisse appeler son vin pétillant du Champagne. </p>
-                ",
-                "<p>answer: an AOC</p>
-                <p>Unlike the AOP which is a European appellation, the AOC is French. This appellation protects the French tradition and prevents anyone from calling their sparkling wine Champagne.</p>
-                ");
-
                 $answer3 = $_POST['question-3-answers'];
-                $para3 = array("<p>réponse: Non (+1 million d’habitants)</p>
-                <p>Et non, le champagne n’est pas une région très peuplée. Pas très étonnant lorsque l’on connaît la place que prennent les vignes champenoises. </p>
-                ",
-                "<p>answer: No (+1 million inhabitants)</p>
-                <p>And no, Champagne is not a very populated region. Not very surprising when you know how much space the Champagne vineyards take up.</p>
-                ");
-
                 $answer4 = $_POST['question-4-answers'];
-                $para4 = array("<p>réponse: Vrai</p>
-                <p>Et oui La région est mondialement reconnue, elle fut intégrée en 2015. Pour plus de détail un article patrimoine existe sur notre site-web.</p>
-                ",
-                "<p>answer: True</p>
-                <p>Yes, the region is world-renowned and was included in 2015. For more details a heritage article exists on our website.</p>
-                ");
-
                 $answer5 = $_POST['question-5-answers'];
-                $para5 = array("<p>réponse: Nord-Est</p>
-                <p>Cette belle région se trouve à 120 km de Paris. Elle s’étend de Maux jusqu'à Epernay et empiète sur la Marne, l’Aube  et l’Aisne</p>
-                ",
-                "<p>answer: North East</p>
-                <p>This beautiful region is 120 km from Paris. It stretches from Maux to Epernay and encroaches on the Marne, Aube and Aisne</p>
-                ");
-
                 $answer6 = $_POST['question-6-answers'];
-                $para6 = array("<p>réponses : Marché de Noël / Fêtes Johanniques.</p>
-                <p>Comme partout, il y a de grands marchés de Noël en Champagne. MAis le plus intéressant sont les fêtes johanniques qui rendent hommage à Jeanne d’Arc et qui fêtent le sacre de Charle VII à Reims, une des plus grandes villes de la Champagne.</p>
-                ",
-                "<p>answers: Christmas Market / Johannine Festivals.</p>
-                <p>Like everywhere, there are great Christmas markets in Champagne. But the most interesting are the Johannine festivals which pay tribute to Joan of Arc and celebrate the coronation of Charles VII in Reims, one of the largest cities in Champagne.</p>
-                ");
-
                 $answer7 = $_POST['question-7-answers'];
-                $para7 = array("<p>réponse: Bleu Or Blanc</p>
-                <p>Ces couleurs représentent bien la Champagne n’est-ce pas? Les deux bandes dorées qu’on appellent des potences symbolisent toutes les châtellenies qui ont constitué le comté de Champagne.</p>
-                ",
-                "<p>answer: Blue Gold White</p>
-                <p>These colours represent Champagne, don't they? The two golden stripes, which are called “potence”, symbolise all the castellanies that made up the county of Champagne.</p>
-                ");
-
-                $answer8 = $_POST['question-8-answers'];
-                $para8 = array("<p>réponse: 231 millions</p>
-                <p>Ce nombre impressionnant montre bien la dévotion des artisans champenois à leur beau métier.</p>
-                ",
-                "<p>answer: 231 million</p>
-                <p>This impressive number shows the devotion of the Champagne craftsmen to their beautiful profession.</p>
-                ");
-
+                $answer8 = $_POST['question-8-answers']
                 $answer9 = $_POST['question-9-answers'];
-                $para9 = array("<p>réponse:34 000 hectares</p>
-                <p>Soit 84 000 terrains de foot. C’est beaucoup.</p>
-                ",
-                "<p>answer:34,000 hectares</p>
-                <p>That's 84,000 football fields. That's a lot of land.</p>
-                ");
-
                 $answer10 = $_POST['question-10-answers'];
-                $para10 = array("<p>réponse: Vrai</p>
-                <p>Et oui la pape Urbain II est née en 1042 à Lagery, commune Champenoise.</p>
-                ",
-                "<p>answer: True</p>
-                <p>Yes, Pope Urban II was born in 1042 in Lagery, a commune in the Champagne region.</p>
-                ");
+                
             }
             $totalCorrect = 0;
 ?>
@@ -121,28 +54,7 @@
                     }
                 ?>
                 <li class = <?=$color?>>
-                    <h3>Quel est le chef-lieu de la Champagne?</h3>
-                    
-                    <div>
-                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
-                        <label for="question-1-answers-A">A) Chalon en Champagne </label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
-                        <label for="question-1-answers-B">B) Reims</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
-                        <label for="question-1-answers-C">C) Troie</label>
-                    </div>
-
-                    <div>
-                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
-                        <label for="question-2-answers-D">D) Epernay</label>
-                    </div>
-                
+                    <?=$question1[$langue]?>
                     <?= $para1[$langue] ?>
                 </li>
                 
@@ -154,28 +66,7 @@
                     }
                 ?>
                 <li class = <?=$color?>>
-                
-                    <h3>Le champagne (boisson) est :</h3>
-                    
-                    <div>
-                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
-                        <label for="question-2-answers-A">A) une AOP (appellation d’origine protégée)</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
-                        <label for="question-2-answers-B">B) une AOC (appellation d’origine contrôlée)</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
-                        <label for="question-2-answers-C">C) une AOC (appellation d'origine champenoise)</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
-                        <label for="question-2-answers-D">D) Juste un vin qui fait des bulles</label>
-                    </div>
+                    <?=$question2[$langue]?>
                     <?= $para2[$langue] ?>
                 </li>
                 
@@ -187,25 +78,8 @@
                     }
                 ?>
                 <li class = <?=$color?>>
-                
-                    <h3>La champagne est elle une grande région?</h3>
-                    
-                    <div>
-                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
-                        <label for="question-3-answers-A">A) Oui (+10 million s d'habitants)</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
-                        <label for="question-3-answers-B">B) Moyennement (+5 millions d'habitants)</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
-                        <label for="question-3-answers-C">C) Non (+1 million d'habitants)</label>
-                    </div>
+                    <?=$question3[$langue]?>
                     <?= $para3[$langue] ?>
-                
                 </li>
                 
                 <?php 
@@ -215,28 +89,7 @@
                     }
                 ?>
                 <li class = <?=$color?>>
-                
-                    <h3>La champagne est situé:</h3>
-                    
-                    <div>
-                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
-                        <label for="question-4-answers-A">A) <img src="../image/cartequiz1.png"></label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
-                        <label for="question-4-answers-B">B) <img src="../image/cartequiz2.png"></label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
-                        <label for="question-4-answers-C">C)  <img src="../image/cartequiz3.png"></label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
-                        <label for="question-4-answers-D">D)  <img src="../image/cartequiz4.png"></label>
-                    </div>
+                    <?=$question4[$langue]?>
                     <?= $para4[$langue] ?>
                 </li>
                
@@ -248,28 +101,7 @@
                     }
                 ?>
                 <li class = <?=$color?>>
-                
-                    <h3>Les couleurs  de la région Champagne-Ardennes sont:</h3>
-                    
-                    <div>
-                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
-                        <label for="question-5-answers-A">A) Bleu Or Blanc</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
-                        <label for="question-5-answers-B">B) Bleu Blanc Rouge</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
-                        <label for="question-5-answers-C">C) Vert</label>
-                    </div>
-                    
-                    <div>
-                        <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
-                        <label for="question-5-answers-D">D) Rouge Jaune</label>
-                    </div>
+                    <?=$question5[$langue]?>
                     <?= $para5[$langue] ?>
                 </li>
                 
@@ -281,20 +113,9 @@
                     }
                 ?>
                 <li class = <?=$color?>>
-                
-                <h3>La Champagne est reconnu patrimoine mondiale de l’Unesco:</h3>
-                
-                <div>
-                    <input type="radio" name="question-6-answers" id="question-6-answers-A" value="A" />
-                    <label for="question-6-answers-A">A) VRAI</label>
-                </div>
-                
-                <div>
-                    <input type="radio" name="question-6-answers" id="question-6-answers-B" value="B" />
-                    <label for="question-6-answers-B">B) FAUX</label>
-                </div>
-                <?= $para6[$langue] ?>
-            </li>
+                    <?=$question6[$langue]?>
+                    <?= $para6[$langue] ?>
+                </li>
             
 
             <?php 
@@ -304,23 +125,7 @@
                 }
             ?>
             <li class = <?=$color?>>
-            
-                <h3>Combien de bouteilles de Champagne ont été produites en 2020?</h3>
-                
-                <div>
-                    <input type="radio" name="question-7-answers" id="question-7-answers-A" value="A" />
-                    <label for="question-7-answers-A">A) 200 000</label>
-                </div>
-                
-                <div>
-                    <input type="radio" name="question-7-answers" id="question-7-answers-B" value="B" />
-                    <label for="question-7-answers-B">B) 2640</label>
-                </div>
-
-                <div>
-                    <input type="radio" name="question-7-answers" id="question-7-answers-C" value="C" />
-                    <label for="question-7-answers-C">C) 231 millions</label>
-                </div>
+                <?=$question7[$langue]?>
                 <?= $para7[$langue] ?>
             </li>
             
@@ -332,28 +137,7 @@
                 }
             ?>
             <li class = <?=$color?>>
-            
-                <h3>La surface de vigne représente approximativement:</h3>
-                
-                <div>
-                    <input type="radio" name="question-8-answers" id="question-8-answers-A" value="A" />
-                    <label for="question-8-answers-A">A) 1 millions hectare</label>
-                </div>
-                
-                <div>
-                    <input type="radio" name="question-8-answers" id="question-8-answers-B" value="B" />
-                    <label for="question-8-answers-B">B) 34 000 hectares</label>
-                </div>
-                
-                <div>
-                    <input type="radio" name="question-8-answers" id="question-8-answers-C" value="C" />
-                    <label for="question-8-answers-C">C) 100 hectares</label>
-                </div>
-                
-                <div>
-                    <input type="radio" name="question-8-answers" id="question-8-answers-D" value="D" />
-                    <label for="question-8-answers-D">D) 58 hectares</label>
-                </div>
+                <?=$question8[$langue]?>
                 <?= $para8[$langue] ?>
             </li>
             
@@ -365,18 +149,7 @@
                 }
             ?>
             <li class = <?=$color?>>
-            
-                <h3>Le pape Urbain II (qui lança les premières croisades) est née en Champagne:</h3>
-                
-                <div>
-                    <input type="radio" name="question-9-answers" id="question-9-answers-A" value="A" />
-                    <label for="question-9-answers-A">A) VRAI</label>
-                </div>
-                
-                <div>
-                    <input type="radio" name="question-9-answers" id="question-9-answers-B" value="B" />
-                    <label for="question-9-answers-B">B) FAUX</label>
-                </div>
+                <?=$question9[$langue]?>
                 <?= $para9[$langue] ?>
             </li>
             
@@ -388,27 +161,7 @@
                 }
             ?>
             <li class = <?=$color?>>
-                    <h3>Quels événements ont lieu en Champagne:</h3>
-                    
-                    <div>
-                        <input type='checkbox' name='question-10-answers[]' id="question-10-answers-A" value='A' /> 
-                        <label for="question-10-answers-A">A) Marché de Noel</label>
-                    </div>
-                    
-                    <div>
-                        <input type="checkbox" name="question-10-answers[]" id="question-10-answers-B" value="B" />
-                        <label for="question-10-answers-B">B) Corrida</label>
-                    </div>
-
-                    <div>
-                        <input type="checkbox" name="question-10-answers[]" id="question-10-answers-C" value="C" />
-                        <label for="question-10-answers-C">C) Fêtes Johanniques</label>
-                    </div>
-
-                    <div>
-                        <input type="checkbox" name="question-10-answers[]" id="question-10-answers-D" value="D" />
-                        <label for="question-10-answers-D">D) Festival de l'Ivresse</label>
-                    </div>
+                <?=$question10[$langue]?>
                 <?= $para10[$langue] ?>
             </li>
             
